5d033e14857e5b35fc5b13c06f32e628f71b64fb
Samantha <samanthagottlieb31@gmail.com>
2020-10-21 23:43:25 +0100

    Modify method to print number of each student

--- directory.rb
+++ directory.rb
@@ -1,19 +1,18 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+
   students = []
-  # get the first name
+
   name = gets.chomp
-  # while the name is not empty, repeat this code
+
   while !name.empty? do
-    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+
     name = gets.chomp
   end
-  # return the array of students
+
   students
 end
 
@@ -23,8 +22,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -33,7 +32,7 @@ def print_footer(students)
 end
 
 students = input_students
-# nothing happens until we call the methods
+
 print_header
 print(students)
 print_footer(students)
